08820e618c7d3fd649537a3831da77e3087fef4a
PookMook <arthur.juchereau@gmail.com>
2019-12-22 00:03:30 -0500

    ghetto test

--- test/test.js
+++ test/test.js
@@ -1,22 +1,19 @@
 const GON = require('../lib/index')
-//console.log(GON.parse(`{"helloWorld":true}`))
-//console.log(GON.parse(`{"helloWorld":null}`))
 
+console.log('')
+console.log('test basic parse')
+console.log(GON.parse(`{"helloWorld":true}`))
+console.log(GON.parse(`{"helloWorld":null}`))
+console.log('')
 
-const objectToSerialize = {
-  name:"arthur", 
-  deeperObj:{
-    name:"deep",
-    nestedArray:[null,"test"]
-  }, 
-  deeperArray:[
-    'test',
-    {name:"nested"}
-  ]
-}
-
-//console.log(`test createRef immutability:
-//`)
-//console.log(GON.stringify(objectToSerialize,null,1))
+console.log(`test createRef object immutability:`)
+const objectToSerialize = {name:"test",nest:{deep:[]},deep:[{nest:true}]}
 console.log(GON.stringify(objectToSerialize))
 console.log(GON.stringify(objectToSerialize))
+console.log('')
+
+console.log(`test createRef array immutability:`)
+const arrayToSerialize = ["test",{deep:[]},[{nest:true}]]
+console.log(GON.stringify(arrayToSerialize))
+console.log(GON.stringify(arrayToSerialize))
+console.log('')
